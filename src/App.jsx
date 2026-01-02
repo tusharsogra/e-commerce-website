582816b5651686a7f2e83d8f9161ee0a10979446
tusharsogra <tusharsogra2007@gmail.com>
2026-01-02 16:57:24 +0530

    button and app

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Contact from './components/Contact'
-import Services from './components/Services'
 import About from './components/About'
 import { Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-size-[20px_20px]">
+    <div className=" absolute top-0 z-[-2] h-screen w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-size-[20px_20px]">
       <Navbar />
        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
+        
       </Routes>
     </div>
   )
